ee0c99a1af3bace6cecedae8ec81e26ac58a821f
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-05-12 11:04:17 +0530

    removed predefined tasks

--- ToDoJs.js
+++ ToDoJs.js
@@ -9,23 +9,28 @@ function cutTaskFn(event){
 //To add new list item by creating them ONE BY ONE
 function addFn(){
     var aTT=  document.getElementById("addTaskText");
+    localStorage.setItem("task1",aTT.value);
 
+    // The container for the new elements
     var newDiv= document.createElement("div");
     newDiv.className="list";
     document.getElementById("listContainer").appendChild(newDiv);
 
+    // The new checkbox
     var checkB= document.createElement("input");
     checkB.type="checkbox";
     checkB.onclick=cutTaskFn;
     newDiv.appendChild(checkB);
 
+    // The new textbox
     var textB= document.createElement("input");
     textB.type="text";
     textB.size="50";
-    textB.value=aTT.value;
+    textB.value= localStorage.getItem("task1");
     aTT.value="";
     newDiv.appendChild(textB);
 
+    // The new delete button
     var delB= document.createElement("img");
     delB.src="delBtn.png";
     delB.onclick=delTask;
